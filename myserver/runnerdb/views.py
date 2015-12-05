7377894170494dc2f1b815fc0f6234e9e81cc477
AlexB <ab2053@cam.ac.uk>
2015-12-05 02:32:12 +0000

    Changed views

--- myserver/runnerdb/views.py
+++ myserver/runnerdb/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from models import Runner
+from django.shortcuts import render_to_response
+#from polls.models import Poll
+from django.http import JsonResponse
 
+from Data import Runners
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the runner_id index.")
+def send_location(request):
+    runner = json.loads(request.body)
+    Runners[runner.id] = runner
+def get_location():
+    return JsonResponse(Runners) 
+def send_donation(request):
+    pass
