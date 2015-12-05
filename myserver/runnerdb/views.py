326ca5f6342c036d67275ada91ddc5177683b8b1
AlexB <ab2053@cam.ac.uk>
2015-12-05 02:38:21 +0000

    Added the ring henry amazing shit

--- myserver/runnerdb/views.py
+++ myserver/runnerdb/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render_to_response
+﻿from django.shortcuts import render_to_response
 #from polls.models import Poll
 from django.http import JsonResponse
 
 from Data import Runners
+from twilio.rest import TwilioRestClient
+
 
 def send_location(request):
     runner = json.loads(request.body)
@@ -10,4 +12,15 @@ def send_location(request):
 def get_location():
     return JsonResponse(Runners) 
 def send_donation(request):
-    pass
+    #This temporary idea demonstrates how to implement a vibration on a fitbit. The target phone is called, causing the fitbit
+    #to vibrate
+    account = "ACe1eb373d270917b737deaedb1ecda797"
+    token = "c4909e963577d964791ef521a1195fb1"
+    print("Setting up client")
+    client = TwilioRestClient(account, token)
+
+    print("Trying to make a call")
+    call = client.calls.create(to="447933144812",
+                                from_="441163260756",
+                                url="https://demo.twilio.com/welcome/voice/")
+    print(call.sid)
