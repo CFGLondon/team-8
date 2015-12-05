cf939413439cdb5854620e37d390203d3e868d11
AlexB <ab2053@cam.ac.uk>
2015-12-05 02:58:30 +0000

    Hopefully we can ring him now!

--- myserver/runnerdb/urls.py
+++ myserver/runnerdb/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+﻿from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     # ex: /runnerdb/
     url(r'^$', views.index, name='index'),
-    # ex: /runnerdb/5/send_donation
-    url(r'^(?P<runner_id>[0-9]+)/send_donation/$', views.send_donation, name='send_donation'),
-    # ex: /runnerdb/5/results/
-    #url(r'^(?P<runner_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /runnerdb/5/vote/
-    #url(r'^(?P<runner_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    # ex: /runnerdb/send_donation
+    url(r'^/send_donation/$', views.send_donation, name='send_donation'),
+    # ex: /runnerdb/get_location
+    url(r'^/get_location/$', views.get_location, name='get_location'),
+    # ex: /runnerdb/send_location
+    url(r'^/send_location/$', views.send_location, name='send_location'),
 ]
